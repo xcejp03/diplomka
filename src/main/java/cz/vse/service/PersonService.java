461cc129a0b17406ce816fc672e6b4c6e95f6fe2
pcejka <petr.cejka@inventi.cz>
2016-10-09 10:04:55 +0200

    implementováno person service

--- src/main/java/cz/vse/service/PersonService.java
+++ src/main/java/cz/vse/service/PersonService.java
@@ -2,14 +2,20 @@ package cz.vse.service;
 
 import cz.vse.dao.PersonDao;
 import cz.vse.entity.Person;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by pcejka on 03.10.2016.
  */
 @Service
 public class PersonService {
+    private final Logger l = Logger.getLogger(this.getClass());
+    @Autowired
+    PersonDao personDao;
 //    @Autowired
 //    private PersonDao personDao;
 //
@@ -21,5 +27,47 @@ public class PersonService {
 //        }
 //
 //    }
+
+    public void createPerson(Person person) {
+        l.debug("creating person - service");
+        personDao.savePerson(person);
+        l.info("created person - service: " + person);
+    }
+
+    public void updatePerson(Person person) {
+        l.debug("updating person - service");
+        personDao.updatePerson(person);
+        l.info("updated person - service: " + person);
+    }
+
+    public void deletePerson(Person personToDelete) {
+        l.debug("deleting person - service");
+        personDao.deletePerson(personToDelete);
+        l.info("person deleted - service: " + personToDelete);
+    }
+
+    public void deletePersonById(long personToDeleteById) {
+        l.debug("deleting person - service");
+        Person personToDelete;
+        personToDelete = personDao.getPersonById(personToDeleteById);
+        personDao.deletePerson(personToDelete);
+        l.info("person deleted - service: " + personToDelete);
+    }
+
+    public Person findPersonById(long id) {
+        l.debug("finding person - service");
+        Person person;
+        person = personDao.getPersonById(id);
+        l.info("person founf - service: " + id + " - " + person);
+        return person;
+    }
+
+    public List<Person> findAllPersons() {
+        l.debug("finding all persons - service");
+        List<Person> personList;
+        personList = personDao.getAllPersons();
+        l.info("found all persons - service: " + personList.toString());
+        return personList;
+    }
 
 }
