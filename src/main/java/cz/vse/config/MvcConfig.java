fa71340137c05adfaefff748ac473ec9476beac4
pcejka <petr.cejka@inventi.cz>
2016-12-22 19:44:16 +0100

    pokus o udělání static public - neúspěšně (nechano zakomentováno)

--- src/main/java/cz/vse/config/MvcConfig.java
+++ src/main/java/cz/vse/config/MvcConfig.java
@@ -6,6 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+//    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
--- src/main/java/cz/vse/config/WebSecurityConfig.java
+++ src/main/java/cz/vse/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/home", "/project/**", "/test/thym").permitAll()
+                .antMatchers("/", "/home", "/project/**", "/static/**", "/test/thym").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
