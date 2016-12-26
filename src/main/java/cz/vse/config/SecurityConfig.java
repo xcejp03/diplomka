2f51c4ea389ab8718f01b823ad57b879741f8ee6
pcejka <petr.cejka@inventi.cz>
2016-12-26 14:08:42 +0100

    pročištěno přihlašování SecurityConfig a importy u login a projects

--- src/main/java/cz/vse/config/SecurityConfig.java
+++ src/main/java/cz/vse/config/SecurityConfig.java
@@ -31,11 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .usersByUsernameQuery(
-//                        "select username,password, enabled from person where username=?")
-//                .authoritiesByUsernameQuery(
-//                        "select username, role from user_role where username=?");
     }
 
     @Override
@@ -43,9 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
 //                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .antMatchers( "/login", "/home", "/index", "/resources/**", "/static/**", "/", "/test/thym", "/thyme", "../script.js", "/script.js", "/css/**",
-                        "/resources/static/**", "/resources/static", "/resources/static/css", "/resources/static/css/**").permitAll()
-                .antMatchers( "/login", "/home", "/index", "/", "/test/thym", "/thyme", "/font-awesome/**", "/font-awesome/", "/font-awesome").permitAll()
+                .antMatchers("/login", "/home", "/index", "/", "/thyme", "/script.js").permitAll()
+                .antMatchers("/test/thym", "/font-awesome/**" ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login")
@@ -61,53 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
 }
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//            /*auth.inMemoryAuthentication().withUser("mkyong").password("123456").roles("USER");
-//            auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
-//            auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");*/
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-///*
-//        http.authorizeRequests()
-//                .antMatchers("/admin*/
-///**").access("hasRole('ROLE_ADMIN')")
-// .antMatchers("/dba*/
-///**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
-// .and().formLogin();
-// */
-//        http
-//                .formLogin()
-//                .loginPage("/login.html")
-//                .failureUrl("/login-error.html")
-//                .and()
-//                .logout()
-//                .logoutSuccessUrl("/index.html");
-//
-////        http.authorizeRequests().antMatchers("/admin/**")
-////                .access("hasRole('ROLE_ADMIN')").and().formLogin()
-////                .loginPage("/login").failureUrl("/login?error")
-////                .usernameParameter("username")
-////                .passwordParameter("password")
-////                .and().logout().logoutSuccessUrl("/login?logout")
-////                .and().csrf()
-////                .and().exceptionHandling().accessDeniedPage("/403");
-//    }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        PasswordEncoder encoder = new BCryptPasswordEncoder();
-//        return encoder;
-//    }
-//}
