65217e8932a0cca21f7750253361e5e14fd373f0
pcejka <petr.cejka@inventi.cz>
2016-12-26 23:23:39 +0100

    mock bootstrapu z test controlleru přewsunut do root controlleru a poupraveno reálná templates

--- src/main/java/cz/vse/controller/TestSuiteController.java
+++ src/main/java/cz/vse/controller/TestSuiteController.java
@@ -44,7 +44,7 @@ public class TestSuiteController {
         model.addAttribute("listPersons", personService.findAllPersons());
         model.addAttribute("listTcMusters", tcMusterService.findAllTestCaseMusters());
 
-        return "suite";
+        return "suiteCreate";
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
