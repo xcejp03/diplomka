42b1034a0c136d80bc966d3bfe1b15bfa20fdc7e
pcejka <petr.cejka@inventi.cz>
2017-04-17 11:48:44 +0200

    vyčištěno od zakomentovaného kódu - šablony

--- src/main/java/cz/vse/config/ConfigConstants.java
+++ src/main/java/cz/vse/config/ConfigConstants.java
@@ -9,5 +9,5 @@ public class ConfigConstants {
     public static final String REPOSITORIES_PACKAGE = "cz.vse.repository";
     public static final String ENTITIES_PACKAGE = "cz.vse.entity";
     public static final String LOCAL_DATE_PATTERN = "yyyy-MM-dd";
-    public static final String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+//    public static final String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 }
